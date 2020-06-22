43a3245e312f90b7d48d116bfc703beb4260a144
naokihayasan127 <65150371+naokihayasan127@users.noreply.github.com>
2020-06-22 17:49:43 +0900

    自動更新機能

---
+++ app/assets/javascripts/modules/message.js
@@ -0,0 +1,74 @@
+$(function(){
+
+  function buildHTML(message){
+    if(message.image){
+    let img = ( message.image )? `<img class= "form_image" src=${message.image}> `: ''
+    let html = 
+              `<div class="messagebox" data-message-id=${message.id}>
+                <div class="post-info">
+                  <div class="post-info__name">
+                    ${message.user_name}
+                  </div>
+                  <div class="post-info__date">
+                    ${message.created_at}
+                  </div>
+                </div>
+                <div class="post-message">
+                  <p class="Message__content">
+                    ${message.content}
+                  </p>
+                  ${img}
+                </div>           
+              </div>`
+    return html;  
+  }
+   else {
+    let html = 
+              `<div class="messagebox" data-message-id=${message.id}>
+                <div class="post-info">
+                  <div class="post-info__name">
+                    ${message.user_name}
+                  </div>
+                  <div class="post-info__date">
+                    ${message.created_at}
+                  </div>
+                </div>
+                <div class="post-message">
+                  <p class="Message__content">
+                    ${message.content}
+                  </p>
+                </div>
+              </div>`
+    return html;
+  };
+}
+                
+$('.form').on('submit', function(e){
+  e.preventDefault();
+  let formData = new FormData(this);
+  let url = $(this).attr('action');
+  
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
+  })
+  .done(function(data){
+    let chatmain = '.chat-main__post-list';
+    let html = buildHTML(data);
+    $(chatmain).append(html);
+    $(chatmain).animate({ scrollTop: $(chatmain)[0].scrollHeight});
+    $('form')[0].reset();
+    $('.submit').prop('disabled', false);
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+     })
+     .always(function(){
+      $('.submit').prop('disabled', false);
+    });
+   });
+});
